3e77f4fb9792f80f7900dc78db96edf6f52fbb58
NexusI9 <nassim.elkhantour@gmail.com>
2026-05-07 10:54:10 +0800

    split root bridge loop into clas

--- Assets/Prefabs/IslandBuilder/Scripts/IslandBuilder.cs
+++ Assets/Prefabs/IslandBuilder/Scripts/IslandBuilder.cs
@@ -10,14 +10,12 @@ namespace Island
 {
     public class IslandBuilder : MonoBehaviour
     {
-        [Serializable]
-        public class RootProperty
+
+        public enum RootCreateMethod
         {
-            public int Segments = 30;
-            public float Depth = 1.0f;
-            public float Shrink = 0.4f;
-            public bool Smooth;
-        }
+            BridgeLoop,
+            MeshNet
+        };
 
         [Header("Builder")]
         [SerializeField] private GameObject chunkPrefab;
@@ -27,14 +25,16 @@ namespace Island
         [SerializeField] private float scrollSensitivity = 1f;
         [SerializeField] private float radiusPadding = 0f;
 
-        [Header("Island")]
-        [SerializeField] private RootProperty Belt;
-        [SerializeField] private RootProperty Bottom;
-	
+        [Header("Root")]
+        [SerializeField] private RootCreateMethod _rootCreateMethod = RootCreateMethod.BridgeLoop;
+        [SerializeField] private RootBridgeLoop _rootBridgeLoop = new();
+
+
         [Header("Controls")]
         [SerializeField] private KeyCode spawnKey = KeyCode.P;
         [SerializeField] private KeyCode outlineKey = KeyCode.L;
         [SerializeField] private KeyCode deleteKey = KeyCode.Delete;
+
 
         private List<GameObject> _spawnedChunks = new();
         private List<RadialMask> _circlesMask = new();
@@ -42,23 +42,15 @@ namespace Island
         private bool _isPlacingNew = false;
         private bool _isMovingExisting = false;
 
+
         [SerializeField] private Island islandPrefab;
         private Island _island;
 
-        // Inner mesh parts cache, used during the noise process
-        private struct RootParts
-        {
-            public Mesh Top;    // Root Top Part
-            public Mesh Bottom; // Root Bottom Part
-            public Mesh Cap;    // Root Cap Part
-        };
-
-        private RootParts _rootParts;
 
         void Awake()
         {
             _island = Instantiate(islandPrefab);
-	    _island.Init();
+            _island.Init();
         }
 
         void Update()
@@ -131,10 +123,7 @@ namespace Island
             if (Input.GetKeyDown(outlineKey) && _currentActiveChunk == null)
             {
                 GenerateGround();
-                GenerateRoot(_island.GetGroundMesh());
-                Noise();
-                CombineRoot();
-		_island.BakeTexture();
+                GenerateRoot();
             }
         }
 
@@ -176,149 +165,29 @@ namespace Island
             _island.SetGroundMesh(outlined);
         }
 
-        /// <summary>
-        /// Generate the island root according to the ground mesh.
-        /// </summary>
-        /// <remarks>
-        /// For the island's root we translate, shrink down the ground mesh and
-        /// bridges the different parts together.
-        /// </remarks>
-        /// <returns>The final root mesh.</returns>
-        private void GenerateRoot(Mesh baseMesh)
+        private void GenerateRoot()
         {
-            // 1. Offset and shrink the root strates (belt, bootom)
-            Mesh topLoop = MeshUtils.Clone(baseMesh);
-            // topLoop = MeshUtils.Decimate(topLoop, 1.0f);
-            Shrink(topLoop, _circlesMask, 3 * Belt.Shrink);
-            MeshUtils.OffsetVertices(topLoop, new Vector3(0, -Belt.Depth, 0));
 
-            _rootParts.Cap = MeshUtils.Clone(baseMesh);
-            // _rootParts.Cap = MeshUtils.Decimate(topLoop, 1.3f);
-            Shrink(_rootParts.Cap, _circlesMask, 3 * Bottom.Shrink);
-            MeshUtils.OffsetVertices(_rootParts.Cap, new Vector3(0, -Bottom.Depth, 0));
+            switch (_rootCreateMethod)
+            {
+                case RootCreateMethod.BridgeLoop:
+                    _rootBridgeLoop.Generate(_island.GetGroundMesh(), _circlesMask);
+                    _rootBridgeLoop.Noise(_island.GetGroundMesh(), _island.GetRootMesh(), _circlesMask, _island.transform);
+                    _island.SetRootMesh(_rootBridgeLoop.GetCombinedMesh());
+                    break;
 
-            // 2. Bridges the parts together
-            _rootParts.Top = MeshBridge.CreateBridgeByProximity(baseMesh.vertices, topLoop.vertices);
-            _rootParts.Bottom = MeshBridge.CreateBridgeByProximity(topLoop.vertices, _rootParts.Cap.vertices);
+                case RootCreateMethod.MeshNet:
+                    break;
+            }
 
-            Normal.Flip(_rootParts.Cap);
+            _island.BakeTexture();
+
         }
+
 
         // -------------------------------------------------------------------------
         // Helper
         // -------------------------------------------------------------------------
-
-        private void CombineRoot()
-        {
-            CombineInstance[] combine = new CombineInstance[3];
-            combine[0].mesh = _rootParts.Top;
-            combine[1].mesh = _rootParts.Bottom;
-            combine[2].mesh = _rootParts.Cap;
-
-            Mesh combinedMesh = new Mesh();
-            combinedMesh.Clear();
-            combinedMesh.CombineMeshes(combine, true, false);
-            combinedMesh.RecalculateBounds();
-            combinedMesh.RecalculateNormals();
-
-            _island.SetRootMesh(combinedMesh);
-        }
-
-        private void Noise()
-        {
-
-            Mesh groundMesh = _island.GetGroundMesh();
-            Mesh rootMesh = _island.GetRootMesh();
-
-            List<NoiseJob> jobs = new List<NoiseJob>
-        {
-		// Ground -> Belt Begin
-		new NoiseJob
-        {
-            Name = "Ground to Belt",
-            Noise = new NoiseSettings
-            {
-            Mode = NoiseMode.XZRadial,
-            Amplitude = 0.6f
-            },
-            RadialMasks = _circlesMask,
-            Items = new List<NoiseItem>
-            {
-            new NoiseItem
-            {
-                Mesh = groundMesh,
-                Transform = _island.Ground.transform,
-                Range = (0, groundMesh.vertexCount)
-            },
-            new NoiseItem
-            {
-                Mesh = _rootParts.Top,
-                Transform = _island.Root.transform,
-                Range = (0, groundMesh.vertexCount)
-            }
-            }
-        },
-		
-		// Belt End -> Bottom Begin
-		new NoiseJob
-        {
-            Name = "Belt End to Bottom Begin",
-            Noise = new NoiseSettings
-            {
-            Mode = NoiseMode.XZRadialPlusY,
-            Amplitude = 0.5f,
-            YAmplitude = 0.4f
-            },
-            RadialMasks = _circlesMask,
-            Items = new List<NoiseItem>
-            {
-            new NoiseItem
-            {
-                Mesh = _rootParts.Top,
-                Transform = _island.Root.transform,
-                Range = (groundMesh.vertexCount, _rootParts.Top.vertexCount)
-            },
-            new NoiseItem
-            {
-                Mesh = _rootParts.Bottom,
-                Transform = _island.Root.transform,
-                Range = (0, _rootParts.Top.vertexCount - groundMesh.vertexCount)
-            }
-            }
-        },
-		
-		// Bottom end -> Cap
-		new NoiseJob
-        {
-            Name = "Bottom End to Cap",
-            Noise = new NoiseSettings
-            {
-            Mode = NoiseMode.PureY,
-            YAmplitude = 0.8f
-            },
-            RadialMasks = _circlesMask,
-            Items = new List<NoiseItem>
-            {
-            new NoiseItem
-            {
-                Mesh = _rootParts.Bottom,
-                Transform = _island.Root.transform,
-                Range = (_rootParts.Top.vertexCount - groundMesh.vertexCount, _rootParts.Bottom.vertexCount)
-            },
-            new NoiseItem
-            {
-                Mesh = _rootParts.Cap,
-                Transform = _island.Root.transform,
-                Range = (0, _rootParts.Cap.vertexCount)
-            }
-            }
-        }
-        };
-
-
-            NoiseIsland.Apply(jobs);
-
-        }
 
         /// <summary>
         /// Calculates a 2D Rect encompassing a list of circles (position + radius).
@@ -394,59 +263,6 @@ namespace Island
             return false;
         }
 
-        /// <summary>
-        /// Shrink the mesh vertices based on a distance-filed.
-        /// </summary>
-        /// <remarks>
-        /// For eact mesh vertices we check their distance on each radial masks. The
-        /// closer, the harder the pull.
-        /// </remarks>
-        private static void Shrink(Mesh mesh, List<RadialMask> srcMasks,
-                                   float strength)
-        {
-
-            // Subdivide our masks with average points for more accurate distance field
-            List<RadialMask> masks = AddMiddleAnchor(srcMasks);
-            List<Vector3> vertices = mesh.vertices.ToList();
-            List<Vector3> result = new(vertices.Count);
-
-            foreach (var v in vertices)
-            {
-                Vector3 total = Vector3.zero;
-                float totalWeight = 0f;
-
-                foreach (var m in masks)
-                {
-                    Vector3 toCenter =
-                        new Vector3(m.Position.x - v.x, 0f, m.Position.y - v.z);
-
-                    float dist = toCenter.magnitude;
-                    float radius = Mathf.Max(m.Radius, 0.0001f);
-
-                    // smooth decay (never fully zero)
-                    float t = dist / radius;
-                    float weight = 1f / (1f + t * t);
-
-                    if (dist > 0.0001f)
-                        total += toCenter.normalized * weight;
-
-                    totalWeight += weight;
-                }
-
-                Vector3 final = v;
-
-                if (totalWeight > 0f)
-                {
-                    Vector3 avg = total / totalWeight;
-                    final += avg * strength;
-                }
-
-                result.Add(final);
-            }
-
-            mesh.vertices = result.ToArray();
-            mesh.RecalculateBounds();
-        }
 
         /// <summary>
         /// Dedicated internal method to triangulate the island levels and clean
@@ -460,45 +276,6 @@ namespace Island
             mesh.SetTriangles(triangles, 0);
         }
 
-        /// <summary>
-        /// Generate additional intermediate masks
-        /// placed at the midpoint between every pair of existing masks.
-        /// </summary>
-        /// <remarks>
-        /// The function does not modify the original masks but returns a new list
-        /// containing both the original masks and the generated midpoint anchors.
-        /// The midpoint mask position is computed as the average of two mask
-        /// positions, and its radius is the average of both radii.
-        ///
-        /// Note: This operation has O(n²) complexity and may significantly increase
-        /// the number of masks for large input lists.
-        ///
-        /// Usecase: We mostly use this function to make the distance-field shrinking
-        /// process more accurate and keep the vertices inside.
-        /// </remarks>
-        public static List<RadialMask> AddMiddleAnchor(List<RadialMask> masks)
-        {
-            var result = new List<RadialMask>(masks);
-
-            int count = masks.Count;
-
-            for (int i = 0; i < count; i++)
-            {
-                for (int j = i + 1; j < count; j++)
-                {
-                    RadialMask a = masks[i];
-                    RadialMask b = masks[j];
-
-                    Vector2 midPos = (a.Position + b.Position) * 0.5f;
-
-                    float midRadius = (a.Radius + b.Radius) * 0.5f;
-
-                    result.Add(new RadialMask { Position = midPos, Radius = midRadius });
-                }
-            }
-
-            return result;
-        }
 
         // -------------------------------------------------------------------------
         // Input Events Handlers
---
+++ Assets/Prefabs/IslandBuilder/Scripts/RootBridgeLoop.cs
@@ -0,0 +1,277 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+using System.Linq;
+
+namespace Island
+{
+    [Serializable]
+    public class RootBridgeLoop
+    {
+
+        [Serializable]
+        public class RootProperty
+        {
+            public int Segments = 30;
+            public float Depth = 1.0f;
+            public float Shrink = 0.4f;
+            public bool Smooth;
+        }
+
+
+        [SerializeField] private RootProperty Belt;
+        [SerializeField] private RootProperty Bottom;
+
+
+        // Inner mesh parts cache, used during the noise process
+        public class Meshes
+        {
+            public Mesh Top;    // Root Top Part
+            public Mesh Bottom; // Root Bottom Part
+            public Mesh Cap;    // Root Cap Part
+        }
+
+        public Meshes Parts = new();
+
+
+        /// <summary>
+        /// Generate the island root according to the ground mesh.
+        /// </summary>
+        /// <remarks>
+        /// For the island's root we translate, shrink down the ground mesh and
+        /// bridges the different parts together.
+        /// </remarks>
+        public void Generate(Mesh baseMesh, List<RadialMask> circlesMask)
+        {
+            // 1. Offset and shrink the root strates (belt, bootom)
+            Mesh topLoop = MeshUtils.Clone(baseMesh);
+            // topLoop = MeshUtils.Decimate(topLoop, 1.0f);
+            Shrink(topLoop, circlesMask, 3 * Belt.Shrink);
+            MeshUtils.OffsetVertices(topLoop, new Vector3(0, -Belt.Depth, 0));
+
+            Parts.Cap = MeshUtils.Clone(baseMesh);
+            // _rootParts.Cap = MeshUtils.Decimate(topLoop, 1.3f);
+            Shrink(Parts.Cap, circlesMask, 3 * Bottom.Shrink);
+            MeshUtils.OffsetVertices(Parts.Cap, new Vector3(0, -Bottom.Depth, 0));
+
+            // 2. Bridges the parts together
+            Parts.Top = MeshBridge.CreateBridgeByProximity(baseMesh.vertices, topLoop.vertices);
+            Parts.Bottom = MeshBridge.CreateBridgeByProximity(topLoop.vertices, Parts.Cap.vertices);
+
+            Normal.Flip(Parts.Cap);
+        }
+
+        public Mesh GetCombinedMesh()
+        {
+            CombineInstance[] combine = new CombineInstance[3];
+            combine[0].mesh = Parts.Top;
+            combine[1].mesh = Parts.Bottom;
+            combine[2].mesh = Parts.Cap;
+
+            Mesh combinedMesh = new Mesh();
+            combinedMesh.Clear();
+            combinedMesh.CombineMeshes(combine, true, false);
+            combinedMesh.RecalculateBounds();
+            combinedMesh.RecalculateNormals();
+
+            return combinedMesh;
+        }
+
+
+        public void Noise(Mesh groundMesh, Mesh rootMesh, List<RadialMask> circlesMask, Transform transform)
+        {
+
+            List<NoiseJob> jobs = new List<NoiseJob>
+        {
+		// Ground -> Belt Begin
+		new NoiseJob
+        {
+            Name = "Ground to Belt",
+            Noise = new NoiseSettings
+            {
+            Mode = NoiseMode.XZRadial,
+            Amplitude = 0.6f
+            },
+            RadialMasks = circlesMask,
+            Items = new List<NoiseItem>
+            {
+            new NoiseItem
+            {
+                Mesh = groundMesh,
+                Transform = transform,
+                Range = (0, groundMesh.vertexCount)
+            },
+            new NoiseItem
+            {
+                Mesh = Parts.Top,
+                Transform = transform,
+                Range = (0, groundMesh.vertexCount)
+            }
+            }
+        },
+		
+		// Belt End -> Bottom Begin
+		new NoiseJob
+        {
+            Name = "Belt End to Bottom Begin",
+            Noise = new NoiseSettings
+            {
+            Mode = NoiseMode.XZRadialPlusY,
+            Amplitude = 0.5f,
+            YAmplitude = 0.4f
+            },
+            RadialMasks = circlesMask,
+            Items = new List<NoiseItem>
+            {
+            new NoiseItem
+            {
+                Mesh = Parts.Top,
+                Transform = transform,
+                Range = (groundMesh.vertexCount, Parts.Top.vertexCount)
+            },
+            new NoiseItem
+            {
+                Mesh = Parts.Bottom,
+                Transform = transform,
+                Range = (0, Parts.Top.vertexCount - groundMesh.vertexCount)
+            }
+            }
+        },
+		
+		// Bottom end -> Cap
+		new NoiseJob
+        {
+            Name = "Bottom End to Cap",
+            Noise = new NoiseSettings
+            {
+            Mode = NoiseMode.PureY,
+            YAmplitude = 0.8f
+            },
+            RadialMasks = circlesMask,
+            Items = new List<NoiseItem>
+            {
+            new NoiseItem
+            {
+                Mesh = Parts.Bottom,
+                Transform = transform,
+                Range = (Parts.Top.vertexCount - groundMesh.vertexCount, Parts.Bottom.vertexCount)
+            },
+            new NoiseItem
+            {
+                Mesh = Parts.Cap,
+                Transform = transform,
+                Range = (0, Parts.Cap.vertexCount)
+            }
+            }
+        }
+        };
+
+
+            NoiseIsland.Apply(jobs);
+
+        }
+
+        /// <summary>
+        /// Shrink the mesh vertices based on a distance-filed.
+        /// </summary>
+        /// <remarks>
+        /// For eact mesh vertices we check their distance on each radial masks. The
+        /// closer, the harder the pull.
+        /// </remarks>
+        private static void Shrink(Mesh mesh, List<RadialMask> srcMasks,
+                                   float strength)
+        {
+
+            Debug.Log(mesh);
+            Debug.Log(srcMasks);
+
+            // Subdivide our masks with average points for more accurate distance field
+            List<RadialMask> masks = AddMiddleAnchor(srcMasks);
+            List<Vector3> vertices = mesh.vertices.ToList();
+            List<Vector3> result = new(vertices.Count);
+
+            foreach (var v in vertices)
+            {
+                Vector3 total = Vector3.zero;
+                float totalWeight = 0f;
+
+                foreach (var m in masks)
+                {
+                    Vector3 toCenter =
+                        new Vector3(m.Position.x - v.x, 0f, m.Position.y - v.z);
+
+                    float dist = toCenter.magnitude;
+                    float radius = Mathf.Max(m.Radius, 0.0001f);
+
+                    // smooth decay (never fully zero)
+                    float t = dist / radius;
+                    float weight = 1f / (1f + t * t);
+
+                    if (dist > 0.0001f)
+                        total += toCenter.normalized * weight;
+
+                    totalWeight += weight;
+                }
+
+                Vector3 final = v;
+
+                if (totalWeight > 0f)
+                {
+                    Vector3 avg = total / totalWeight;
+                    final += avg * strength;
+                }
+
+                result.Add(final);
+            }
+
+            mesh.vertices = result.ToArray();
+            mesh.RecalculateBounds();
+        }
+
+
+
+        /// <summary>
+        /// Generate additional intermediate masks
+        /// placed at the midpoint between every pair of existing masks.
+        /// </summary>
+        /// <remarks>
+        /// The function does not modify the original masks but returns a new list
+        /// containing both the original masks and the generated midpoint anchors.
+        /// The midpoint mask position is computed as the average of two mask
+        /// positions, and its radius is the average of both radii.
+        ///
+        /// Note: This operation has O(n²) complexity and may significantly increase
+        /// the number of masks for large input lists.
+        ///
+        /// Usecase: We mostly use this function to make the distance-field shrinking
+        /// process more accurate and keep the vertices inside.
+        /// </remarks>
+        public static List<RadialMask> AddMiddleAnchor(List<RadialMask> masks)
+        {
+            var result = new List<RadialMask>(masks);
+
+            int count = masks.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = i + 1; j < count; j++)
+                {
+                    RadialMask a = masks[i];
+                    RadialMask b = masks[j];
+
+                    Vector2 midPos = (a.Position + b.Position) * 0.5f;
+
+                    float midRadius = (a.Radius + b.Radius) * 0.5f;
+
+                    result.Add(new RadialMask { Position = midPos, Radius = midRadius });
+                }
+            }
+
+            return result;
+        }
+
+
+    }
+
+}
